486d035de45229fe8491903875de36cb45525342
Stefan Boca <stefan.r.boca@gmail.com>
2024-10-29 20:32:38 -0700

    fix: remove copy-paste error

--- lua/cmp/init.lua
+++ lua/cmp/init.lua
@@ -7,7 +7,6 @@ for k, v in pairs(require('cmp.types.cmp')) do
   cmp[k] = v
 end
 cmp.lsp = require('cmp.types.lsp')
-cmp.vim = require('cmp.types.vim')
 
 function cmp.register_source(name, s)
   require('blink.compat.registry').register_source(name, s)
