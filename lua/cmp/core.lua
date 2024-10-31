6fb91c4122121c3f13396e00d4052200aaf92f55
Stefan Boca <stefan.r.boca@gmail.com>
2024-10-31 14:26:17 -0700

    fix: correctly import blink.cmp in cmp.core.view.visible

--- lua/cmp/core.lua
+++ lua/cmp/core.lua
@@ -1,6 +1,6 @@
 local core = {}
 
 core.view = {}
-function core.view.visible() return require('blink.cmp.windows.autocomplete').win:is_open() end
+function core.view.visible() return require('blink.cmp').windows.autocomplete.win:is_open() end
 
 return core
