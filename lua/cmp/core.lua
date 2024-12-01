096ee2d20eadd25410af263608b12362a4d45f27
Stefan Boca <stefan.r.boca@gmail.com>
2024-11-30 22:45:46 -0800

    fix: cmp.core.view.visible

--- lua/cmp/core.lua
+++ lua/cmp/core.lua
@@ -1,7 +1,7 @@
 local core = {}
 
 core.view = {}
-function core.view.visible() return require('blink.cmp').windows.autocomplete.win:is_open() end
+function core.view.visible() return require('blink.cmp.completion.windows.menu').win:is_open() end
 
 core.event = require('blink.compat.event')
 
