0fcc94eb9aa899e7453ea00f6f85ff6b66d8c0e9
Stefan Boca <stefan.r.boca@gmail.com>
2024-12-02 19:53:45 -0800

    fix(cmp.types.lsp): clone tables, and follow nvim-cmp for CompletionItemKind

--- lua/cmp/types/lsp.lua
+++ lua/cmp/types/lsp.lua
@@ -1,3 +1,5 @@
+local utils = require('blink.compat.lib.utils')
+
 local lsp = {}
 
 lsp.PositionEncodingKind = {
@@ -6,16 +8,19 @@ lsp.PositionEncodingKind = {
   UTF32 = 'utf-32',
 }
 
-lsp.CompletionTriggerKind = vim.lsp.protocol.CompletionTriggerKind
-lsp.InsertTextFormat = vim.lsp.protocol.InsertTextFormat
+lsp.CompletionTriggerKind = utils.shallow_copy(vim.lsp.protocol.CompletionTriggerKind)
+lsp.InsertTextFormat = utils.shallow_copy(vim.lsp.protocol.InsertTextFormat)
 
 lsp.InsertTextMode = {
   AsIs = 1,
   AdjustIndentation = 2,
 }
 
-lsp.MarkupKind = vim.lsp.protocol.MarkupKind
-lsp.CompletionItemTag = vim.lsp.protocol.CompletionTag
-lsp.CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+lsp.MarkupKind = utils.shallow_copy(vim.lsp.protocol.MarkupKind)
+lsp.CompletionItemTag = utils.shallow_copy(vim.lsp.protocol.CompletionTag)
+lsp.CompletionItemKind = utils.shallow_copy(vim.lsp.protocol.CompletionItemKind)
+for k, v in pairs(lsp.CompletionItemKind) do
+  lsp.CompletionItemKind[v] = k
+end
 
 return lsp
