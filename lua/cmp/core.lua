f8cd536e03e3513603e25a8347edeb531bbf581d
songliyu <songliyu@tencent.com>
2025-01-19 10:36:13 +0800

    Add default core.filter to prevent lsp_signature's error.
    Fix

--- lua/cmp/core.lua
+++ lua/cmp/core.lua
@@ -5,4 +5,10 @@ function core.view.visible() return require('blink.cmp.completion.windows.menu')
 
 core.event = require('blink.compat.event')
 
+core.filter = core.filter or {
+  sync = function(_, timeout_)
+    vim.wait(timeout_ or 1000, function() end, 10)
+  end,
+}
+
 return core
